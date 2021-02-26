40e160a97138ce56c56678590b341717072f0be6
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-26 14:54:45 +0000

    installed react-router-dom, created Home and PizzaForm components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Route from 'react-router-dom';
+
+function Home(props) {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Route from 'react-router-dom';
+
+function PizzaForm(props) {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default PizzaForm;
